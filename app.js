fc1d978430f36015e7daee380b38159c64971d60
Esdras  Bezerra da Silva <esdrasilva@gmail.com>
2020-01-24 07:52:59 -0300

    Projeto base para POC VRWikiBot

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+
+const port = 3000;
+
+  const AssistantV2 = require('ibm-watson/assistant/v2');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  const assistant = new AssistantV2({
+    version: '2019-02-28',
+    authenticator: new IamAuthenticator({
+      apikey: 'osi3yjXqwlv0XaYH69p_1T_o8I17RYeni-vADVOWhFIb',
+    }),
+    url: 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/814cfdb8-c63b-460f-8d9f-61a5e57ebfa3',
+  });
+
+  const createSession = await assistant.createSession({
+    assistantId: '6da533ac-fb4f-4f19-a658-7c76711d36c9'
+  });
+
+  let sessionId = createSession.result.session_id;
+
+  app.post('/conversation/', (req, res) => {
+    const { text, context = {} } = req.body;
+
+    const params = {
+      input: { text: text },
+      assistantId: '6da533ac-fb4f-4f19-a658-7c76711d36c9',
+      context,
+      sessionId,
+    }
+
+    assistant.message(params, (err, response) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json(err);
+      } else {
+        res.json(response);
+      }
+    });
+  });
+
+  app.get('/conversation/:text*?', (req, res) => {
+    const { text } = req.params;
+    res.json(text);
+  });
+
+app.listen(port, () => console.log(`Running on port ${port}`));
---
+++ app2.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+
+const port = 3000;
+
+async(rep, res) => {
+  const AssistantV1= require('ibm-watson/assistant/v1');
+
+  var assistant = new AssistantV1({
+    username: process.env.ASSISTANT_USERNAME,
+    password: process.env.ASSISTANT_PASSWORD,
+    version: '2018-02-16'
+  });
+
+  app.post('/conversation/', (req, res) => {
+    const { text, context = {} } = req.body;
+
+    const params = {
+      input: { text: text },
+      workspace_id: '6da533ac-fb4f-4f19-a658-7c76711d36c9',
+      context,
+    }/
+
+    assistant.message(params, (err, response) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json(err);
+      } else {
+        res.json(response);
+      }
+    });
+  });
+}
+
+app.listen(port, () => console.log(`Running on port ${port}`));
